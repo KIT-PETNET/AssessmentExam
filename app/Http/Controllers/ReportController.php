c169cf708a1ccc0c2df0377504e6e2c3f9fdc2d0
Icy <kenneth.tadeja@petnet.com.ph>
2024-07-12 11:15:31 +0800

    /reports/ logic/mathematics fix. Added categorization by type

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -11,80 +11,140 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class ReportController extends Controller
 {
-    // Function to calculate commission per transaction
-    public function calculateCommissionPerTransaction()
+    // Function to calculate commission per transaction and save to database
+    public function calculateAndSaveCommissions()
     {
-        $transactions = Data::all(); 
+        $transactions = Data::all();
 
         $n_com_rate = 18;
         $commission_rate = 15;
 
         foreach ($transactions as $transaction) {
-            $charges = $transaction->charges;
-            
+            $charges = $transaction->Charges;
+
             $gross_commission = $charges * ($commission_rate / 100);
             $commission = $gross_commission * (($commission_rate / 100) / ($n_com_rate / 100));
 
-            // Save the calculated commission
+            // Round the calculated values to two decimal places
+            $gross_commission = round($gross_commission, 2);
+            $commission = round($commission, 2);
+
+            // Add calculated values to the transaction object
+            $transaction->gross_commission = $gross_commission;
+            $transaction->commission = $commission;
+            $transaction->net_commission = $commission;
+            $transaction->vatable = $this->isVatable($transaction->Type);
+
+            // Save to database
             SummaryOfTransaction::create([
-                'transaction_id' => $transaction->id,
-                'commission' => $commission,
-                'gross_commission' => $gross_commission,
-                'net_commission' => $commission,
-                'vatable' => $transaction->vatable ?? false 
+                'Type' => $transaction->Type,
+                'Name' => $transaction->Name,
+                'Description' => $transaction->Description,
+                'commission' => $transaction->commission,
+                'gross_commission' => $transaction->gross_commission,
+                'net_commission' => $transaction->net_commission,
+                'vatable' => $transaction->vatable
             ]);
         }
 
         return response()->json(['message' => 'Commissions calculated and saved'], 200);
     }
 
-    // Function to save commissions to the database
-    public function saveCommissionsToDatabase()
+    // Function to compute gross and net commission and generate report
+    public function computeAndGenerateReport(Request $request)
     {
-        return $this->calculateCommissionPerTransaction();
-    }
+        // Hardcoded charge rates and VAT values based on type
+        $chargeRates = [
+            'MTS' => 0.12,
+            'RS' => 0.19,
+            'DR' => 0.15,
+            'IT' => 0.18,
+            'MMT' => 0.13,
+        ];
 
-    // Function to compute gross and net commission
-    public function computeGrossAndNetCommission(Request $request)
-    {
-        $charge_rate = $request->input('charge_rate', 10); 
-        $vat = $request->input('vat', 12); 
+        $vatValues = [
+            'MTS' => 0.12,
+            'RS' => 0.12,
+            'DR' => 0.12,
+            'IT' => 0.12,
+            'MMT' => 0.12,
+        ];
 
         $summary = DB::table('summary_of_transactions')
             ->select(
+                'Type',
                 DB::raw('SUM(commission) as sum_of_commission_per_type'),
                 DB::raw('SUM(CASE WHEN vatable THEN commission ELSE 0 END) as sum_of_vatable_commission'),
                 DB::raw('SUM(CASE WHEN NOT vatable THEN commission ELSE 0 END) as sum_of_non_vatable_commission')
             )
-            ->first();
+            ->groupBy('Type')
+            ->get();
 
-        $charge_rate = $charge_rate / 100;
-        $vat = $vat / 100;
+        $results = [];
+        $totalGrossCommissionVatable = 0;
+        $totalNetCommissionVatable = 0;
+        $totalGrossCommissionNonVatable = 0;
+        $totalNetCommissionNonVatable = 0;
 
-        // Vatable
-        $gross_commission_vatable = $summary->sum_of_vatable_commission / (1 + $vat);
-        $expanded_withholding_tax_vatable = $summary->sum_of_vatable_commission * $charge_rate;
-        $net_commission_vatable = $gross_commission_vatable - $expanded_withholding_tax_vatable;
+        foreach ($summary as $transaction) {
+            $type = $transaction->Type;
+            $charge_rate = $chargeRates[$type];
+            $vat = $vatValues[$type];
 
-        // Not Vatable
-        $gross_commission_non_vatable = $summary->sum_of_non_vatable_commission;
-        $expanded_withholding_tax_non_vatable = $summary->sum_of_non_vatable_commission * $charge_rate;
-        $net_commission_non_vatable = $gross_commission_non_vatable - $expanded_withholding_tax_non_vatable;
+            // Vatable
+            if ($transaction->sum_of_vatable_commission > 0) {
+                $gross_commission_vatable = $transaction->sum_of_vatable_commission / (1 + $vat);
+                $expanded_withholding_tax_vatable = $transaction->sum_of_vatable_commission * $charge_rate;
+                $net_commission_vatable = $gross_commission_vatable - $expanded_withholding_tax_vatable;
 
-        return response()->json([
-            'total_gross_commission_vatable' => $gross_commission_vatable,
-            'total_net_commission_vatable' => $net_commission_vatable,
-            'total_gross_commission_non_vatable' => $gross_commission_non_vatable,
-            'total_net_commission_non_vatable' => $net_commission_non_vatable
-        ], 200);
+                $results[$type]['total_gross_commission_vatable'] = round($gross_commission_vatable, 2);
+                $results[$type]['total_net_commission_vatable'] = round($net_commission_vatable, 2);
+
+                // Add to total
+                $totalGrossCommissionVatable += $gross_commission_vatable;
+                $totalNetCommissionVatable += $net_commission_vatable;
+            }
+
+            // Not Vatable
+            if ($transaction->sum_of_non_vatable_commission > 0) {
+                $gross_commission_non_vatable = $transaction->sum_of_non_vatable_commission;
+                $expanded_withholding_tax_non_vatable = $transaction->sum_of_non_vatable_commission * $charge_rate;
+                $net_commission_non_vatable = $gross_commission_non_vatable - $expanded_withholding_tax_non_vatable;
+
+                $results[$type]['total_gross_commission_non_vatable'] = round($gross_commission_non_vatable, 2);
+                $results[$type]['total_net_commission_non_vatable'] = round($net_commission_non_vatable, 2);
+
+                // Add to total
+                $totalGrossCommissionNonVatable += $gross_commission_non_vatable;
+                $totalNetCommissionNonVatable += $net_commission_non_vatable;
+            }
+        }
+
+        // Add totals for all transaction types
+        $results['total_all_types'] = [
+            'total_gross_commission_vatable' => round($totalGrossCommissionVatable, 2),
+            'total_net_commission_vatable' => round($totalNetCommissionVatable, 2),
+            'total_gross_commission_non_vatable' => round($totalGrossCommissionNonVatable, 2),
+            'total_net_commission_non_vatable' => round($totalNetCommissionNonVatable, 2),
+        ];
+
+        // Save totals to the database
+        DB::table('summary_of_transactions')->update([
+            'total_gross_commission_vatable' => round($totalGrossCommissionVatable, 2),
+            'total_net_commission_vatable' => round($totalNetCommissionVatable, 2),
+            'total_gross_commission_non_vatable' => round($totalGrossCommissionNonVatable, 2),
+            'total_net_commission_non_vatable' => round($totalNetCommissionNonVatable, 2),
+        ]);
+
+        // Generate and download the report as an Excel file
+        $transactions = SummaryOfTransaction::all();
+        return Excel::download(new TransactionsExport($transactions, $results), 'transactions_report.xlsx');
     }
 
-    // Function to generate and download the report as an Excel file
-    public function generateReport()
+    // Helper function to determine if a transaction type is vatable
+    private function isVatable($type)
     {
-        $transactions = SummaryOfTransaction::all();
-
-        // Use Maatwebsite Excel to create an Excel file
-        return Excel::download(new TransactionsExport($transactions), 'transactions_report.xlsx');
+        $vatableTypes = ['MTS', 'IT', 'MMT'];
+        return in_array($type, $vatableTypes);
     }
 }
